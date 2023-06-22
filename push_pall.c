5e157eca469647a6ea884ed4392f97425cb3c5c3
bouzrbay <atighmarcelline@gmail.com>
2023-06-22 16:20:29 -0700

    betty

--- push_pall.c
+++ push_pall.c
@@ -7,22 +7,22 @@
  */
 void m_push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 {
-  stack_t *top;
-  (void)line_number;
+stack_t *top;
+(void)line_number;
 
-  top = malloc(sizeof(stack_t));
-  if (top == NULL)
-    {
-      fprintf(stderr, "Error: malloc failed\n");
-      exit(EXIT_FAILURE);
-    }
+top = malloc(sizeof(stack_t));
+if (top == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
 
-  top->n = var_global.push_arg;
-  top->next = *stack;
-  top->prev = NULL;
-  if (*stack != NULL)
-    (*stack)->prev = top;
-  *stack = top;
+top->n = var_global.push_arg;
+top->next = *stack;
+top->prev = NULL;
+if (*stack != NULL)
+(*stack)->prev = top;
+*stack = top;
 }
 
 /**
@@ -32,14 +32,14 @@ void m_push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
  */
 void m_pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 {
-  stack_t *runner;
+stack_t *runner;
 
-  runner = *stack;
-  while (runner != NULL)
-    {
-      printf("%d\n", runner->n);
-      runner = runner->next;
-    }
+runner = *stack;
+while (runner != NULL)
+{
+printf("%d\n", runner->n);
+runner = runner->next;
+}
 }
 
 /**
@@ -50,13 +50,13 @@ void m_pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
  */
 void m_pint(stack_t **stack, unsigned int line_number)
 {
-  stack_t *runner;
+stack_t *runner;
 
-  runner = *stack;
-  if (runner == NULL)
-    {
-      fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-      exit(EXIT_FAILURE);
-    }
-  printf("%d\n", runner->n);
+runner = *stack;
+if (runner == NULL)
+{
+fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", runner->n);
 }
