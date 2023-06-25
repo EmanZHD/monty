829702e335391756ba9fd2d57715bb6af4e8517f
bouzrbay <atighmarcelline@gmail.com>
2023-06-25 15:52:26 +0100

    monty

---
+++ monty.c
@@ -0,0 +1,180 @@
+#include "monty.h"
+
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pall(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int counter);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+
+/**
+ * f_pchar - prints the char at the top of the stack,
+ * followed by a new line
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pchar(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+
+h = *head;
+if (!h)
+{
+fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+if (h->n > 127 || h->n < 0)
+{
+fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+printf("%c\n", h->n);
+}
+
+
+/**
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+(void)counter;
+
+h = *head;
+if (h == NULL)
+return;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+}
+
+/**
+ * f_pint - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pint(stack_t **head, unsigned int counter)
+{
+if (*head == NULL)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", (*head)->n);
+}
+
+/**
+ * f_pop - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pop(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+
+if (*head == NULL)
+{
+fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+*head = h->next;
+free(h);
+}
+
+/**
+ * f_swap - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_swap(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+int len = 0, aux;
+
+h = *head;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+aux = h->n;
+h->n = h->next->n;
+h->next->n = aux;
+}
+
+/**
+ * f_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_add(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+int len = 0, aux;
+
+h = *head;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+aux = h->n + h->next->n;
+h->next->n = aux;
+*head = h->next;
+free(h);
+}
+
+/**
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void) counter;
+(void) head;
+}
